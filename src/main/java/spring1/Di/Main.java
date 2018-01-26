19857b9afb23049389efe1a4646d3fa145cb26db
iqbalsalman <iqbalsalman1707@gmail.com>
2018-01-26 16:59:47 +0700

    Hitung Luas Persegi dan Lingkaran Spring

---
+++ src/main/java/spring1/Di/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spring1.Di;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import spring1.impl.HitungLuaspersegi;
+
+/**
+ *
+ * @author iqbal
+ */
+public class Main {
+    
+      public static void main(String[] args){
+      ApplicationContext context =
+    	  new ClassPathXmlApplicationContext(new String[] {"Beans.xml"});
+
+    	HitungLuaspersegi cust = (HitungLuaspersegi)context.getBean("Hitung");
+    	System.out.println(cust.hitungLuas(500));
+      }
+}
---
+++ src/main/java/spring1/impl/HitungLuaslingkaran.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package spring1.impl;
+
+import spring1.service.HitungService;
+
+/**
+ *
+ * @author iqbal
+ */
+public class HitungLuaslingkaran implements HitungService {
+
+    @Override
+    public double hitungLuas(int parm) {
+       return Math.PI*parm*parm;
+    }
+    
+}
